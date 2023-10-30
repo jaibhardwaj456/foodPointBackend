089cc80cea971388db0c0e520638013571a883af
Jai <jaibhardwaj456@gmail.com>
2023-10-30 14:23:24 +0530

    cors

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const app = express();
 const port = 5000;
 mongoDB();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-type, Accept"
-  );
-  next();
-});
+import cors from "cors";
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
