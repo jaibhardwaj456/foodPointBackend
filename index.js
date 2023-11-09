550357d0f138666db82a69993da762b0f7205293
jaibhardwaj456 <107449720+jaibhardwaj456@users.noreply.github.com>
2023-11-09 17:48:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import cors from "cors";
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
